2e4aa06c0661348beeab72b3c5b054f04be18ce2
nathanShepherd <nateshep@umich.edu>
2016-11-28 14:25:30 -0500

    Implimented powerRule

--- Module.cpp
+++ Module.cpp
@@ -8,7 +8,12 @@ Module interprets Terms to find its derivitive
 #include "Module.h"
 using namespace std;
 
-Term Module::powerRule(Term inTerm) { return inTerm; }
-Term Module::chainRule(Term inTerm) { return inTerm; }
-Term Module::quotientRule(Term inTerm) { return inTerm; }
-
+Term Module::powerRule(Term inTerm) { 
+    inTerm.setCoef() = inTerm.getCoef() * inTerm.getPower();
+    return inTerm; 
+}
+Term Module::chainRule(Term inTerm) { return inTerm; } //not functional in this iteration
+Term Module::quotientRule(Poly inPoly) { 
+    inPoly
+    return inTerm; 
+}
